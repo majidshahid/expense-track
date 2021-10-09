40b4996bbb0d46ae1d7f46e708fbcb5e8723fe9c
majid shahid <majidshahid474@gmail.com>
2021-10-08 20:38:53 -0700

    track expense

--- src/component/body/index.js
+++ src/component/body/index.js
@@ -4,36 +4,76 @@ import {useState} from "react";
 function Body(){
   var [income,setincome]=useState()
   var [expense,setexpense]=useState("")
-  var [balance, setBalance]= useState(0)
+  var [balance,setbalance]=useState(0)
+  var [xpencse,setxpense]=useState(0);
+  var [val,setvalue]=useState("")
+  const[todo,settodo]=useState([])
+var addincome=()=>{
+    var A= [...todo,];
+    A.push(val);
+    settodo(A)
+    }
+  var a =balance-xpencse;
+ 
   function Foo(){
+    var a = +income + +balance;
+    setbalance(a);
+    var A= [...todo,];
+    A.push(val);
+    var c =`${A} : ${a}`
+   console.log(c)
+    settodo(A)
+
+    setincome("")
    
-var a = +income + +balance
-setBalance(a)
-console.log(a)
-   
-    
   }
+  function Expense(){
+var a = +expense + +xpencse;
+setxpense(a)
+setexpense("")
+
+  }
+ 
+ 
     return (
         <div className="container-fluid">
         <div className="row">
           <div className="col-7 ">
             <div className="text-left">
               <h1>INCOME <span id="income"></span></h1>
-          <input type="number" value={income} onChange={(e)=>setincome(e.target.value)} placeholder="Expense" id="incomeinput" />
+          <input type="number" value={income} onChange={(e)=>setincome(e.target.value)} placeholder="Expense" />
+          <input value={val} onChange={(e)=>setvalue(e.target.value)} type="input" placeholder="Source" />
               <button onClick={Foo}>Add</button>
 
 <hr/>
 <h1>EXPENSE <span id="expense"></span></h1>
 <input type="number" value={expense} onChange={(e)=>setexpense(e.target.value)} placeholder="Income" />
+<button onClick={Expense}>Add</button>
             </div>
           </div>
           <div className="col-5">
-          <h1>Your Balance:<span>{balance}</span></h1>
-          <h1></h1>
-<h1>Your Expense:<span>{expense}</span></h1>
+          <h1>Total INCOME :{balance}</h1>
+          <h1>Your Balance:<span>{a}</span></h1>
+          
+<h1 style={{color: "red"}}>Your Expense:<span>{xpencse}</span></h1>
 <h1></h1>
           </div>
         </div>
+
+
+<div>
+<ul>
+{todo.map((v,i)=>{
+    return <li key ={i}>{v}</li>
+  })}
+</ul>
+</div>
+
+
+
+
+
+
       </div>
 
     );
